f89d5b9c4d82d66f5c1d4ecd587b0b9737a1467e
hafizip00 <hafizip00@gmail.com>
2022-09-05 00:39:27 +0500

    Delete Functionality

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, FlatList } from "react-native";
+import GoalItem from "./components/GoalItem";
+import { useState } from "react";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
+  const [goalText, setGoalText] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  function goalInputHandler(e) {
+    setGoalText(e);
+  }
+  function AddGoal() {
+    setGoals((currentGoals) => [
+      ...currentGoals,
+      { text: goalText, id: Math.random().toString() },
+    ]);
+  }
+
+  function deleteHandler(id) {
+    setGoals((currentGoals) => {
+      return currentGoals.filter((goal) => goal.id !== id);
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <GoalInput goalInputHandler={goalInputHandler} AddGoal={AddGoal} />
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={goals}
+          renderItem={(itemData) => {
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                deleteHandler={deleteHandler}
+                id={itemData.item.id}
+              />
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +52,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+
+  goalsContainer: {
+    flex: 5,
   },
 });
---
+++ components/GoalItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+const GoalItem = ({ text, deleteHandler, id }) => {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        onPress={() => deleteHandler(id)}
+        android_ripple={{ color: "#fff" }}
+      >
+        <Text style={styles.goalText}>{text}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    color: "white",
+    padding: 10,
+  },
+});
